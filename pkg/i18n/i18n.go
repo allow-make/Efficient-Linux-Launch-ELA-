3b211fc5a19085d2e7fa458ba75ba3e49217975a
allow-make <ttttt_698@qq.com>
2026-07-14 20:02:16 +0800

    Update i18n.go

--- pkg/i18n/i18n.go
+++ pkg/i18n/i18n.go
@@ -1,16 +1,43 @@
 package i18n
 
+import (
+	"os"
+	"strings"
+)
+
 var currentLang = "zh"
 
 func SetLanguage(lang string) {
-currentLang = lang
+	if lang == "en" || lang == "zh" {
+		currentLang = lang
+	}
+}
+
+func GetLang() string {
+	return currentLang
 }
 
 func T(key string) string {
-if currentLang == "zh" {
-if val, ok := Zh[key]; ok {
-return val
+	var dict map[string]string
+	if currentLang == "zh" {
+		dict = Zh
+	} else {
+		dict = En
+	}
+	if val, ok := dict[key]; ok {
+		return val
+	}
+	return key
 }
-}
-return key
+
+func DetectLang() string {
+	lang := os.Getenv("LANG")
+	if lang != "" && strings.HasPrefix(lang, "zh") {
+		return "zh"
+	}
+	lang = os.Getenv("LANGUAGE")
+	if lang != "" && strings.HasPrefix(lang, "zh") {
+		return "zh"
+	}
+	return "en"
 }
